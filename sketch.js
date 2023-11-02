27784a70d7251a583c2f60a2591b205b4ae2f163
vanshgupta087 <149563907+vanshgupta087@users.noreply.github.com>
2023-11-02 11:36:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+// Teachable Machine
+// The Coding Train / Daniel Shiffman
+// https://thecodingtrain.com/TeachableMachine/1-teachable-machine.html
+// https://editor.p5js.org/codingtrain/sketches/PoZXqbu4v
+
+// The video
+let video;
+// For displaying the label
+let label = "waiting...";
+// The classifier
+let classifier;
+let modelURL = 'https://teachablemachine.withgoogle.com/models/17ZzJhZ4e/';
+
+// STEP 1: Load the model!
+function preload() {
+  classifier = ml5.imageClassifier(modelURL + 'model.json');
+}
+
+
+function setup() {
+  createCanvas(640, 520);
+  // Create the video
+  video = createCapture(VIDEO);
+  video.hide();
+  // STEP 2: Start classifying
+  classifyVideo();
+}
+
+// STEP 2 classify the videeo!
+function classifyVideo() {
+  classifier.classify(video, gotResults);
+}
+
+function draw() {
+  background(0);
+
+  // Draw the video
+  image(video, 0, 0);
+
+  // STEP 4: Draw the label
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  text(label, width / 2, height - 16);
+
+  
+}
+
+// STEP 3: Get the classification!
+function gotResults(error, results) {
+  // Something went wrong!
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // Store the label and classify again!
+  label = results[0].label;
+  classifyVideo();
+}
